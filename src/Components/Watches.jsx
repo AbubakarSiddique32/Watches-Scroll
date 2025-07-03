6c69fb81536954d0480508b7a2a41fd29069048b
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-03 20:01:57 +0500

    first commit

--- src/Components/Watches.jsx
+++ src/Components/Watches.jsx
@@ -51,6 +51,12 @@ const WatchShowcase = () => {
         vitae modi dolorum sit odit laudantium quo incidunt, debitis enim
         distinctio. Sed omnis aspernatur quis inventore est quo qui?
       </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        consequuntur nesciunt culpa possimus architecto enim quaerat pariatur
+        similique ipsam autem asperiores, nisi debitis ipsum sint dolorem iure
+        accusamus rerum quasi.
+      </p>
 
       <div className="flex justify-center items-center gap-6 flex-wrap">
         {watches.map((watch, index) => (
