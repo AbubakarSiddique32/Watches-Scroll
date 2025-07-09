fada342a392134094b7d1157beb93d2cc06a6364
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-09 15:51:11 +0500

    first commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { FaApple } from "react-icons/fa";
-
+import { IoSearchOutline } from "react-icons/io5";
+import { BiShoppingBag } from "react-icons/bi";
 // Updated Details array with object structure for icon and text
 const Details = [
-  { icon: <FaApple />, text: "Apple Store" },
-  { icon: "", text: "Mac" },
-  { icon: "", text: "iPad" },
-  { icon: "", text: "Phone" },
-  { icon: "", text: "Watches" },
+  { icon: <FaApple /> },
+  { text: "Mac" },
+  { text: "iPad" },
+  { text: "Phone" },
+  { text: "Watch" },
+  { text: "AirPods" },
+  { text: "TV & Home" },
+  { text: "Entertainment" },
+  { text: "Accessories" },
+  { text: "Support" },
+  { icon: <IoSearchOutline /> },
+  { icon: <BiShoppingBag /> },
 ];
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="flex">
-        <ul className="flex items-center list-none">
+      <nav className="flex px-[100px] py-[30px]">
+        <ul className="flex items-center list-none gap-10">
           {Details.map((detail, index) => {
             return (
               <li key={index} className="flex no-underline bg-red ">
                 <a href="" className="flex items-center space-x-2">
-                  {detail.icon && <span>{detail.icon}</span>}
+                  {detail.icon && <span className="">{detail.icon}</span>}
                   {/* Render icon if exists */}
                   <span>{detail.text}</span>
                 </a>
