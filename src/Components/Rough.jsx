05d245e1b251f121ff86118fafe849ec698ba49d
AbubakarSiddique32 <abubakartarar918@gmail.com>
2025-07-10 18:50:10 +0500

    first commit

--- src/Components/Rough.jsx
+++ src/Components/Rough.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Rough = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt odit
+        necessitatibus laboriosam eligendi? Quaerat adipisci id et porro
+        officia, labore nihil excepturi nemo harum expedita ratione molestias
+        est voluptatem accusamus?
+      </p>
+    </div>
+  );
 };
 
 export default Rough;
